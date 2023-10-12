91b6fc86aea1681a3cb3ef6198b8ad704bbff214
Nyoman Lanang Kresna Erlangga <dimasnyoman1@gmail.com>
2023-10-12 22:00:13 +0700

    update button project title items

--- src/app/_components/Projects/ProjectsItem.jsx
+++ src/app/_components/Projects/ProjectsItem.jsx
@@ -24,7 +24,7 @@ const Projects = [
     description:
       'The "F1 Standings Tracker" is a web frontend project built using ReactJS that allows Formula 1 enthusiasts to stay updated with the latest drivers standings in the F1 racing season. This dynamic and user-friendly application fetches data from an API source to provide real-time information about the current standings of F1 drivers, their points, and their team affiliations.',
     buttonLink: "https://f1.dimnyan.tech/",
-    buttonTitle: "View Project",
+    buttonTitle: "Visit Project",
     imageUrl: "/images/f1-thumbnail.png",
   },
   {
@@ -32,7 +32,7 @@ const Projects = [
     description:
       "The Penguin Wave Animation project is a fun web animation created using HTML and CSS. It features an adorable penguin character with a waving hand, bringing life to the web page users with its playful movement.",
     buttonLink: "http://dimnyan.000webhostapp.com/Projects/penguinwave.html",
-    buttonTitle: "View Project",
+    buttonTitle: "Visit Project",
     imageUrl: "/images/penguin-thumbnail.png",
   },
 ];
