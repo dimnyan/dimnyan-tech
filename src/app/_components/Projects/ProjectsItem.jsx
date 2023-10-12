c36bb6c375f37f3f31fbab24d0472209485b1c7a
Nyoman Lanang Kresna Erlangga <dimasnyoman1@gmail.com>
2023-10-12 22:55:58 +0700

    update expense app project desc

--- src/app/_components/Projects/ProjectsItem.jsx
+++ src/app/_components/Projects/ProjectsItem.jsx
@@ -4,9 +4,18 @@ import React from "react";
 
 const Projects = [
   {
-    title: "Expenses App - ReactJS",
+    title: "F1 Standings Tracker - ReactJS API",
     description:
-      "This project is apart of my Udemy Course. It includes React components, Props, states, dynamic styling to update expenses. It also includes Filtering by Year and also Animated Graph Chart with the power of react ",
+      'The "F1 Standings Tracker" is a web frontend project built using ReactJS that allows Formula 1 enthusiasts to stay updated with the latest drivers standings in the F1 racing season. This dynamic and user-friendly application fetches data from an API source to provide real-time information about the current standings of F1 drivers, their points, and their team affiliations.',
+    buttonLink: "https://f1.dimnyan.tech/",
+    buttonTitle: "Visit Project",
+    imageUrl: "/images/f1-thumbnail.png",
+  },
+
+  {
+    title: "Expense Tracking App with ReactJS",
+    description:
+      "This project is part of my Udemy course in ReactJS, focusing on the creation of an Expense Tracker App. The core functionality of this app includes expense entry, categorization, and a dynamic chart with real-time updates as users apply filters.",
     buttonLink: "https://github.com/dimnyan/expense-app",
     buttonTitle: "View Source Code",
     imageUrl: "/images/expense-thumbnail.png",
@@ -19,14 +28,7 @@ const Projects = [
     buttonTitle: "View Source Code",
     imageUrl: "/images/landing-thumbnail.png",
   },
-  {
-    title: "F1 Standings Tracker - ReactJS API",
-    description:
-      'The "F1 Standings Tracker" is a web frontend project built using ReactJS that allows Formula 1 enthusiasts to stay updated with the latest drivers standings in the F1 racing season. This dynamic and user-friendly application fetches data from an API source to provide real-time information about the current standings of F1 drivers, their points, and their team affiliations.',
-    buttonLink: "https://f1.dimnyan.tech/",
-    buttonTitle: "Visit Project",
-    imageUrl: "/images/f1-thumbnail.png",
-  },
+
   {
     title: "Penguin Wave Animation - HTML & CSS",
     description:
