12f8cae758c75dc47469674f1f6453ac0d1454f4
Nyoman Lanang Kresna Erlangga <48331946+dimnyan@users.noreply.github.com>
2024-11-27 14:27:52 +0700

    add logo and remove some images not used

--- src/app/_components/Home/Logos.jsx
+++ src/app/_components/Home/Logos.jsx
@@ -2,17 +2,17 @@
 import { motion } from "framer-motion";
 
 const LogosSection = () => {
-  const logos = ["mysql", "github", "nodejs", "react", "next", "mongo"];
+  const logos = ["spring boot","mysql", "github", "nodejs", "react", "next", "golang", "postgresql", "mongo"];
   return (
     <div className="bg-slate-200 py-5 md:py-12 overflow-hidden">
       <div className="flex [mask-image:linear-gradient(to_right,transparent,black,black,transparent)]">
         <motion.div
           className="flex gap-10 md:gap-28 lg:gap-44 lg:pr-44"
           animate={{
-            translateX: "-120%",
+            translateX: "-100%",
           }}
           transition={{
-            duration: 15,
+            duration: 20,
             repeat: Infinity,
             ease: "linear",
             repeatType: "loop",
